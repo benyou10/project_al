1e7e4ee38f3e8328ff610e74d3874e6ef442b97d
benyou10 <ybenzian016@gmail.com>
2025-11-25 20:14:51 +0100

    first push

---
+++ src/main/java/com/example/project_al/ProjectAlApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_al;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectAlApplication.class, args);
+    }
+
+}
